e8a262ec564123a384ce987db8bb6a637c0ba7a5
张雅婷 <1632046291@qq.com>
2026-05-18 11:34:05 +0800

    chore: 初始化前后端项目骨架

---
+++ backend/src/main/java/com/lab/equipment_booking/EquipmentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.lab.equipment_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentBookingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lab/equipment_booking/EquipmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.equipment_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
